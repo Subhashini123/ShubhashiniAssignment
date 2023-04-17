8e0ef6bbd6de852f2a512f5815d2546114c681f4
SSahare <subhashinisahare94@gmail.com>
2023-04-17 09:04:48 +0530

    Add Permissions and class name to manifest file .

--- app/src/main/java/com/example/shubhashiniassignment/AlbumApplication.kt
+++ app/src/main/java/com/example/shubhashiniassignment/AlbumApplication.kt
@@ -1,9 +1,7 @@
 package com.example.shubhashiniassignment
-
 import android.app.Application
 import com.example.shubhashiniassignment.di.AlbumApplicationComponent
 import com.example.shubhashiniassignment.di.DaggerAlbumApplicationComponent
-
 
 class AlbumApplication : Application() {
     lateinit var appComponent: AlbumApplicationComponent
--- app/src/main/java/com/example/shubhashiniassignment/views/AlbumListFragment.kt
+++ app/src/main/java/com/example/shubhashiniassignment/views/AlbumListFragment.kt
@@ -6,7 +6,6 @@ import android.content.Context
 import android.content.res.Configuration
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
